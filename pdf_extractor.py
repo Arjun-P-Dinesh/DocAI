bfa54f922b7786815d3f95ecac817e981bcf01f6
Arjgorthmic <arjunpdinesh123@gmail.com>
2024-07-28 13:04:31 +0530

    update

--- pdf_extractor.py
+++ pdf_extractor.py
@@ -12,10 +12,12 @@ def extract_text(pdf_path):
             img_name = f"{pdf_path[:-4]}_image_{i}.png"
             img.save(f'{os.getcwd()}/{img_name}')  # Saving the image in current directory
         text += page.extractText()
-    # print(text)
+    with open('/home/arjgorthmic/SpecializationProject/temptext.txt', 'w', encoding='utf-8') as f:
+        f.write(text)
     return text
 
 
-# pdfpath = r"C:\Users\HP\Desktop\SpecializationProject\sample 4.pdf"
-# extract_text(pdfpath)
 
+
+pdfpath = "/home/arjgorthmic/SpecializationProject/samplePDF/sample 4.pdf"
+extract_text(pdfpath)
